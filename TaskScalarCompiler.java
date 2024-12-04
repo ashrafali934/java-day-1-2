6ba9d925df20d7a56cf5bf5b7d86a8726ae9d368
shahid <shahidshah77562@gmail.com>
2024-12-04 12:38:17 +0530

    Add files via upload

---
+++ TaskScalarCompiler.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class TaskScalarCompiler
+{
+    public static void main (String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        int b=sc.nextInt();
+        String c=String.format("name=%s\nage=%d",a,b);
+        System.out.println(c);
+    }
+    
+}
