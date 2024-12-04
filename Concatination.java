6f4679f6d839004b726fc707c95e1ed695193b82
shahid <shahidshah77562@gmail.com>
2024-12-04 12:42:35 +0530

    Add files via upload

---
+++ Concatination.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Concatination
+{
+    public static void main (String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        System.out.println("congratulations" +a+ "you won 5 lakhs rupees");
+    }
+}
