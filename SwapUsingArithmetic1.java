50253af935dc0b19b2372eb38cdee94b887f2ede
shahid <shahidshah77562@gmail.com>
2024-12-04 12:39:36 +0530

    Add files via upload

---
+++ SwapUsingArithmetic1.java
@@ -0,0 +1,13 @@
+class SwapUsingArithmetic1
+{
+    public static void main(String[] args)
+    {
+        int a=20;
+        int b=50;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
